e90103c938ff601e3f00523431c221c6410e018a
William Moreira <32074474+willmoreira@users.noreply.github.com>
2025-07-24 18:08:16 -0300

    Initial Commit

---
+++ OnboardingAppTests/OnboardingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingAppTests.swift
+//  OnboardingAppTests
+//
+//  Created by William on 24/07/25.
+//
+
+import Testing
+@testable import OnboardingApp
+
+struct OnboardingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
